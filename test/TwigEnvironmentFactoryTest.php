d77009073ad3c59a261484dfaf71eee7075adc89
Geert Eltink <xtreamwayz@gmail.com>
2016-11-05 11:10:42 +0100

    Update tests

---
+++ test/TwigEnvironmentFactoryTest.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace ZendTest\Expressive\Twig;
+
+use Interop\Container\ContainerInterface;
+use PHPUnit_Framework_TestCase as TestCase;
+use Twig_Environment as TwigEnvironment;
+use Zend\Expressive\Helper\ServerUrlHelper;
+use Zend\Expressive\Helper\UrlHelper;
+use Zend\Expressive\Twig\Exception\InvalidConfigException;
+use Zend\Expressive\Twig\Exception\InvalidExtensionException;
+use Zend\Expressive\Twig\TwigEnvironmentFactory;
+use Zend\Expressive\Twig\TwigExtension;
+
+class TwigEnvironmentFactoryTest extends TestCase
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function setUp()
+    {
+        $this->container = $this->prophesize(ContainerInterface::class);
+    }
+
+    public function testCallingFactoryWithNoConfigReturnsTwigEnvironmentInstance()
+    {
+        $this->container->has('config')->willReturn(false);
+        $this->container->has(ServerUrlHelper::class)->willReturn(false);
+        $this->container->has(UrlHelper::class)->willReturn(false);
+        $factory     = new TwigEnvironmentFactory();
+        $environment = $factory($this->container->reveal());
+
+        $this->assertInstanceOf(TwigEnvironment::class, $environment);
+
+        return $environment;
+    }
+
+    public function testUsesDebugConfiguration()
+    {
+        $config = ['debug' => true];
+        $this->container->has('config')->willReturn(true);
+        $this->container->get('config')->willReturn($config);
+        $this->container->has(ServerUrlHelper::class)->willReturn(false);
+        $this->container->has(UrlHelper::class)->willReturn(false);
+        $factory     = new TwigEnvironmentFactory();
+        $environment = $factory($this->container->reveal());
+
+        $this->assertTrue($environment->isDebug());
+        $this->assertFalse($environment->getCache());
+        $this->assertTrue($environment->isStrictVariables());
+        $this->assertTrue($environment->isAutoReload());
+    }
+
+    /**
+     * @depends testCallingFactoryWithNoConfigReturnsTwigEnvironmentInstance
+     */
+    public function testDebugDisabledSetsUpEnvironmentForProduction(TwigEnvironment $environment)
+    {
+        $this->assertFalse($environment->isDebug());
+        $this->assertFalse($environment->isStrictVariables());
+        $this->assertFalse($environment->isAutoReload());
+    }
+
+    public function testCanSpecifyCacheDirectoryViaConfiguration()
+    {
+        $config = ['templates' => ['cache_dir' => __DIR__]];
+        $this->container->has('config')->willReturn(true);
+        $this->container->get('config')->willReturn($config);
+        $this->container->has(ServerUrlHelper::class)->willReturn(false);
+        $this->container->has(UrlHelper::class)->willReturn(false);
+        $factory     = new TwigEnvironmentFactory();
+        $environment = $factory($this->container->reveal());
+
+        $this->assertEquals($config['templates']['cache_dir'], $environment->getCache());
+    }
+
+    public function testAddsTwigExtensionIfRouterIsInContainer()
+    {
+        $serverUrlHelper = $this->prophesize(ServerUrlHelper::class)->reveal();
+        $urlHelper       = $this->prophesize(UrlHelper::class)->reveal();
+        $this->container->has('config')->willReturn(false);
+        $this->container->has(ServerUrlHelper::class)->willReturn(true);
+        $this->container->get(ServerUrlHelper::class)->willReturn($serverUrlHelper);
+        $this->container->has(UrlHelper::class)->willReturn(true);
+        $this->container->get(UrlHelper::class)->willReturn($urlHelper);
+        $factory     = new TwigEnvironmentFactory();
+        $environment = $factory($this->container->reveal());
+
+        $this->assertTrue($environment->hasExtension('zend-expressive'));
+    }
+
+    public function testUsesAssetsConfigurationWhenAddingTwigExtension()
+    {
+        $config          = [
+            'templates' => [
+                'assets_url'     => 'http://assets.example.com/',
+                'assets_version' => 'XYZ',
+            ],
+        ];
+        $serverUrlHelper = $this->prophesize(ServerUrlHelper::class)->reveal();
+        $urlHelper       = $this->prophesize(UrlHelper::class)->reveal();
+        $this->container->has('config')->willReturn(true);
+        $this->container->get('config')->willReturn($config);
+        $this->container->has(ServerUrlHelper::class)->willReturn(true);
+        $this->container->get(ServerUrlHelper::class)->willReturn($serverUrlHelper);
+        $this->container->has(UrlHelper::class)->willReturn(true);
+        $this->container->get(UrlHelper::class)->willReturn($urlHelper);
+        $factory     = new TwigEnvironmentFactory();
+        $environment = $factory($this->container->reveal());
+        $extension   = $environment->getExtension('zend-expressive');
+
+        $this->assertInstanceOf(TwigExtension::class, $extension);
+        $this->assertAttributeEquals($config['templates']['assets_url'], 'assetsUrl', $extension);
+        $this->assertAttributeEquals($config['templates']['assets_version'], 'assetsVersion', $extension);
+        $this->assertAttributeSame($serverUrlHelper, 'serverUrlHelper', $extension);
+        $this->assertAttributeSame($urlHelper, 'urlHelper', $extension);
+    }
+
+    public function invalidExtensions()
+    {
+        return [
+            'null'                  => [null],
+            'true'                  => [true],
+            'false'                 => [false],
+            'zero'                  => [0],
+            'int'                   => [1],
+            'zero-float'            => [0.0],
+            'float'                 => [1.1],
+            'non-service-string'    => ['not-an-extension'],
+            'array'                 => [['not-an-extension']],
+            'non-extensions-object' => [(object) ['extension' => 'not-an-extension']],
+        ];
+    }
+
+    /**
+     * @dataProvider invalidExtensions
+     */
+    public function testRaisesExceptionForInvalidExtensions($extension)
+    {
+        $config = [
+            'templates' => [
+            ],
+            'twig'      => [
+                'extensions' => [$extension],
+            ],
+        ];
+        $this->container->has('config')->willReturn(true);
+        $this->container->get('config')->willReturn($config);
+        $this->container->has(ServerUrlHelper::class)->willReturn(false);
+        $this->container->has(UrlHelper::class)->willReturn(false);
+
+        if (is_string($extension)) {
+            $this->container->has($extension)->willReturn(false);
+        }
+
+        $factory = new TwigEnvironmentFactory();
+
+        $this->setExpectedException(InvalidExtensionException::class);
+        $factory($this->container->reveal());
+    }
+
+    public function testConfiguresGlobals()
+    {
+        $config          = [
+            'twig' => [
+                'globals' => [
+                    'ga_tracking' => 'UA-XXXXX-X',
+                    'foo'         => 'bar',
+                ],
+            ],
+        ];
+        $serverUrlHelper = $this->prophesize(ServerUrlHelper::class)->reveal();
+        $urlHelper       = $this->prophesize(UrlHelper::class)->reveal();
+        $this->container->has('config')->willReturn(true);
+        $this->container->get('config')->willReturn($config);
+        $this->container->has(ServerUrlHelper::class)->willReturn(true);
+        $this->container->get(ServerUrlHelper::class)->willReturn($serverUrlHelper);
+        $this->container->has(UrlHelper::class)->willReturn(true);
+        $this->container->get(UrlHelper::class)->willReturn($urlHelper);
+        $factory     = new TwigEnvironmentFactory();
+        $environment = $factory($this->container->reveal());
+        $extension   = $environment->getExtension('zend-expressive');
+
+        $this->assertInstanceOf(TwigExtension::class, $extension);
+        $this->assertAttributeEquals($config['twig']['globals'], 'globals', $extension);
+        $this->assertAttributeSame($serverUrlHelper, 'serverUrlHelper', $extension);
+        $this->assertAttributeSame($urlHelper, 'urlHelper', $extension);
+    }
+
+    public function invalidConfiguration()
+    {
+        // @codingStandardsIgnoreStart
+        //                        [Config value,                        Type ]
+        return [
+            'true'             => [true, 'boolean'],
+            'false'            => [false, 'boolean'],
+            'zero'             => [0, 'integer'],
+            'int'              => [1, 'integer'],
+            'zero-float'       => [0.0, 'double'],
+            'float'            => [1.1, 'double'],
+            'string'           => ['not-configuration', 'string'],
+            'non-array-object' => [(object) ['not' => 'configuration'], 'stdClass'],
+        ];
+        // @codingStandardsIgnoreEnd
+    }
+
+    /**
+     * @dataProvider invalidConfiguration
+     */
+    public function testRaisesExceptionForInvalidConfigService($config, $contains)
+    {
+        $this->container->has('config')->willReturn(true);
+        $this->container->get('config')->willReturn($config);
+        $factory = new TwigEnvironmentFactory();
+
+        $this->setExpectedException(InvalidConfigException::class, $contains);
+        $factory($this->container->reveal());
+    }
+}
--- test/TwigRendererFactoryTest.php
+++ test/TwigRendererFactoryTest.php
@@ -15,19 +15,15 @@ use ReflectionProperty;
 use Zend\Expressive\Helper\ServerUrlHelper;
 use Zend\Expressive\Helper\UrlHelper;
 use Zend\Expressive\Template\TemplatePath;
-use Zend\Expressive\Twig\Exception\InvalidConfigException;
-use Zend\Expressive\Twig\Exception\InvalidExtensionException;
-use Zend\Expressive\Twig\TwigExtension;
+use Zend\Expressive\Twig\TwigEnvironmentFactory;
 use Zend\Expressive\Twig\TwigRenderer;
 use Zend\Expressive\Twig\TwigRendererFactory;
-use ZendTest\Expressive\Twig\TestAsset\Extension\FooTwigExtension;
-use ZendTest\Expressive\Twig\TestAsset\Extension\BarTwigExtension;
 
 class TwigRendererFactoryTest extends TestCase
 {
     /**
      * @var ContainerInterface
-    */
+     */
     private $container;
 
     public function setUp()
@@ -39,6 +35,7 @@ class TwigRendererFactoryTest extends TestCase
     {
         $r = new ReflectionProperty($twig, 'template');
         $r->setAccessible(true);
+
         return $r->getValue($twig);
     }
 
@@ -46,12 +43,12 @@ class TwigRendererFactoryTest extends TestCase
     {
         return [
             'foo' => __DIR__ . '/TestAsset/bar',
-            1 => __DIR__ . '/TestAsset/one',
+            1     => __DIR__ . '/TestAsset/one',
             'bar' => [
                 __DIR__ . '/TestAsset/baz',
                 __DIR__ . '/TestAsset/bat',
             ],
-            0 => [
+            0     => [
                 __DIR__ . '/TestAsset/two',
                 __DIR__ . '/TestAsset/three',
             ],
@@ -106,9 +103,16 @@ class TwigRendererFactoryTest extends TestCase
         $this->container->has('config')->willReturn(false);
         $this->container->has(ServerUrlHelper::class)->willReturn(false);
         $this->container->has(UrlHelper::class)->willReturn(false);
+        $environment = new TwigEnvironmentFactory();
+        $this->container->has(TwigEnvironmentFactory::class)->willReturn(true);
+        $this->container->get(TwigEnvironmentFactory::class)->willReturn(
+            $environment($this->container->reveal())
+        );
+
         $factory = new TwigRendererFactory();
-        $twig = $factory($this->container->reveal());
+        $twig    = $factory($this->container->reveal());
         $this->assertInstanceOf(TwigRenderer::class, $twig);
+
         return $twig;
     }
 
@@ -122,95 +126,6 @@ class TwigRendererFactoryTest extends TestCase
         $this->assertEmpty($paths);
     }
 
-    public function testUsesDebugConfigurationToPrepareEnvironment()
-    {
-        $config = ['debug' => true];
-        $this->container->has('config')->willReturn(true);
-        $this->container->get('config')->willReturn($config);
-        $this->container->has(ServerUrlHelper::class)->willReturn(false);
-        $this->container->has(UrlHelper::class)->willReturn(false);
-        $factory = new TwigRendererFactory();
-        $twig = $factory($this->container->reveal());
-
-        $environment = $this->fetchTwigEnvironment($twig);
-
-        $this->assertTrue($environment->isDebug());
-        $this->assertFalse($environment->getCache());
-        $this->assertTrue($environment->isStrictVariables());
-        $this->assertTrue($environment->isAutoReload());
-    }
-
-    /**
-     * @depends testCallingFactoryWithNoConfigReturnsTwigInstance
-     */
-    public function testDebugDisabledSetsUpEnvironmentForProduction(TwigRenderer $twig)
-    {
-        $environment = $this->fetchTwigEnvironment($twig);
-
-        $this->assertFalse($environment->isDebug());
-        $this->assertFalse($environment->isStrictVariables());
-        $this->assertFalse($environment->isAutoReload());
-    }
-
-    public function testCanSpecifyCacheDirectoryViaConfiguration()
-    {
-        $config = ['templates' => ['cache_dir' => __DIR__]];
-        $this->container->has('config')->willReturn(true);
-        $this->container->get('config')->willReturn($config);
-        $this->container->has(ServerUrlHelper::class)->willReturn(false);
-        $this->container->has(UrlHelper::class)->willReturn(false);
-        $factory = new TwigRendererFactory();
-        $twig = $factory($this->container->reveal());
-
-        $environment = $this->fetchTwigEnvironment($twig);
-        $this->assertEquals($config['templates']['cache_dir'], $environment->getCache());
-    }
-
-    public function testAddsTwigExtensionIfRouterIsInContainer()
-    {
-        $serverUrlHelper = $this->prophesize(ServerUrlHelper::class)->reveal();
-        $urlHelper = $this->prophesize(UrlHelper::class)->reveal();
-        $this->container->has('config')->willReturn(false);
-        $this->container->has(ServerUrlHelper::class)->willReturn(true);
-        $this->container->get(ServerUrlHelper::class)->willReturn($serverUrlHelper);
-        $this->container->has(UrlHelper::class)->willReturn(true);
-        $this->container->get(UrlHelper::class)->willReturn($urlHelper);
-
-        $factory = new TwigRendererFactory();
-        $twig = $factory($this->container->reveal());
-
-        $environment = $this->fetchTwigEnvironment($twig);
-        $this->assertTrue($environment->hasExtension('zend-expressive'));
-    }
-
-    public function testUsesAssetsConfigurationWhenAddingTwigExtension()
-    {
-        $config = [
-            'templates' => [
-                'assets_url'     => 'http://assets.example.com/',
-                'assets_version' => 'XYZ',
-            ],
-        ];
-        $serverUrlHelper = $this->prophesize(ServerUrlHelper::class)->reveal();
-        $urlHelper = $this->prophesize(UrlHelper::class)->reveal();
-        $this->container->has('config')->willReturn(true);
-        $this->container->get('config')->willReturn($config);
-        $this->container->has(ServerUrlHelper::class)->willReturn(true);
-        $this->container->get(ServerUrlHelper::class)->willReturn($serverUrlHelper);
-        $this->container->has(UrlHelper::class)->willReturn(true);
-        $this->container->get(UrlHelper::class)->willReturn($urlHelper);
-        $factory = new TwigRendererFactory();
-        $twig = $factory($this->container->reveal());
-
-        $environment = $this->fetchTwigEnvironment($twig);
-        $extension = $environment->getExtension('zend-expressive');
-        $this->assertInstanceOf(TwigExtension::class, $extension);
-        $this->assertAttributeEquals($config['templates']['assets_url'], 'assetsUrl', $extension);
-        $this->assertAttributeEquals($config['templates']['assets_version'], 'assetsVersion', $extension);
-        $this->assertAttributeSame($serverUrlHelper, 'serverUrlHelper', $extension);
-        $this->assertAttributeSame($urlHelper, 'urlHelper', $extension);
-    }
-
     public function testConfiguresTemplateSuffix()
     {
         $config = [
@@ -222,8 +137,13 @@ class TwigRendererFactoryTest extends TestCase
         $this->container->get('config')->willReturn($config);
         $this->container->has(ServerUrlHelper::class)->willReturn(false);
         $this->container->has(UrlHelper::class)->willReturn(false);
+        $environment = new TwigEnvironmentFactory();
+        $this->container->has(TwigEnvironmentFactory::class)->willReturn(true);
+        $this->container->get(TwigEnvironmentFactory::class)->willReturn(
+            $environment($this->container->reveal())
+        );
         $factory = new TwigRendererFactory();
-        $twig = $factory($this->container->reveal());
+        $twig    = $factory($this->container->reveal());
 
         $this->assertAttributeSame($config['templates']['extension'], 'suffix', $twig);
     }
@@ -239,8 +159,13 @@ class TwigRendererFactoryTest extends TestCase
         $this->container->get('config')->willReturn($config);
         $this->container->has(ServerUrlHelper::class)->willReturn(false);
         $this->container->has(UrlHelper::class)->willReturn(false);
+        $environment = new TwigEnvironmentFactory();
+        $this->container->has(TwigEnvironmentFactory::class)->willReturn(true);
+        $this->container->get(TwigEnvironmentFactory::class)->willReturn(
+            $environment($this->container->reveal())
+        );
         $factory = new TwigRendererFactory();
-        $twig = $factory($this->container->reveal());
+        $twig    = $factory($this->container->reveal());
 
         $paths = $twig->getPaths();
         $this->assertPathsHasNamespace('foo', $paths);
@@ -257,135 +182,5 @@ class TwigRendererFactoryTest extends TestCase
         $this->assertPathNamespaceContains(__DIR__ . '/TestAsset/one', null, $paths);
         $this->assertPathNamespaceContains(__DIR__ . '/TestAsset/two', null, $paths);
         $this->assertPathNamespaceContains(__DIR__ . '/TestAsset/three', null, $paths);
-    }
-
-    public function testInjectsCustomExtensionsIntoTwigEnvironment()
-    {
-        $config = [
-            'templates' => [
-            ],
-            'twig' => [
-                'extensions' => [
-                    new FooTwigExtension(),
-                    BarTwigExtension::class,
-                ],
-            ],
-        ];
-        $this->container->has('config')->willReturn(true);
-        $this->container->get('config')->willReturn($config);
-        $this->container->has(ServerUrlHelper::class)->willReturn(false);
-        $this->container->has(UrlHelper::class)->willReturn(false);
-        $this->container->has(BarTwigExtension::class)->willReturn(true);
-        $this->container->get(BarTwigExtension::class)->willReturn(new BarTwigExtension());
-        $factory = new TwigRendererFactory();
-        $view = $factory($this->container->reveal());
-        $this->assertInstanceOf(TwigRenderer::class, $view);
-        $environment = $this->fetchTwigEnvironment($view);
-        $this->assertTrue($environment->hasExtension('foo-twig-extension'));
-        $this->assertInstanceOf(FooTwigExtension::class, $environment->getExtension('foo-twig-extension'));
-        $this->assertTrue($environment->hasExtension('bar-twig-extension'));
-        $this->assertInstanceOf(BarTwigExtension::class, $environment->getExtension('bar-twig-extension'));
-    }
-
-    public function invalidExtensions()
-    {
-        return [
-            'null'                  => [null],
-            'true'                  => [true],
-            'false'                 => [false],
-            'zero'                  => [0],
-            'int'                   => [1],
-            'zero-float'            => [0.0],
-            'float'                 => [1.1],
-            'non-service-string'    => ['not-an-extension'],
-            'array'                 => [['not-an-extension']],
-            'non-extensions-object' => [(object) ['extension' => 'not-an-extension']],
-        ];
-    }
-
-    /**
-     * @dataProvider invalidExtensions
-     */
-    public function testRaisesExceptionForInvalidExtensions($extension)
-    {
-        $config = [
-            'templates' => [
-            ],
-            'twig' => [
-                'extensions' => [ $extension ],
-            ],
-        ];
-        $this->container->has('config')->willReturn(true);
-        $this->container->get('config')->willReturn($config);
-        $this->container->has(ServerUrlHelper::class)->willReturn(false);
-        $this->container->has(UrlHelper::class)->willReturn(false);
-
-        if (is_string($extension)) {
-            $this->container->has($extension)->willReturn(false);
-        }
-
-        $factory = new TwigRendererFactory();
-
-        $this->setExpectedException(InvalidExtensionException::class);
-        $factory($this->container->reveal());
-    }
-
-    public function testConfiguresGlobals()
-    {
-        $config = [
-            'twig' => [
-                'globals' => [
-                    'ga_tracking' => 'UA-XXXXX-X',
-                    'foo' => 'bar',
-                ],
-            ],
-        ];
-        $serverUrlHelper = $this->prophesize(ServerUrlHelper::class)->reveal();
-        $urlHelper = $this->prophesize(UrlHelper::class)->reveal();
-        $this->container->has('config')->willReturn(true);
-        $this->container->get('config')->willReturn($config);
-        $this->container->has(ServerUrlHelper::class)->willReturn(true);
-        $this->container->get(ServerUrlHelper::class)->willReturn($serverUrlHelper);
-        $this->container->has(UrlHelper::class)->willReturn(true);
-        $this->container->get(UrlHelper::class)->willReturn($urlHelper);
-        $factory = new TwigRendererFactory();
-        $twig = $factory($this->container->reveal());
-
-        $environment = $this->fetchTwigEnvironment($twig);
-        $extension = $environment->getExtension('zend-expressive');
-        $this->assertInstanceOf(TwigExtension::class, $extension);
-        $this->assertAttributeEquals($config['twig']['globals'], 'globals', $extension);
-        $this->assertAttributeSame($serverUrlHelper, 'serverUrlHelper', $extension);
-        $this->assertAttributeSame($urlHelper, 'urlHelper', $extension);
-    }
-
-    public function invalidConfiguration()
-    {
-        // @codingStandardsIgnoreStart
-        //                        [Config value,                        Type ]
-        return [
-            'true'             => [true,                                'boolean'],
-            'false'            => [false,                               'boolean'],
-            'zero'             => [0,                                   'integer'],
-            'int'              => [1,                                   'integer'],
-            'zero-float'       => [0.0,                                 'double'],
-            'float'            => [1.1,                                 'double'],
-            'string'           => ['not-configuration',                 'string'],
-            'non-array-object' => [(object) ['not' => 'configuration'], 'stdClass'],
-        ];
-        // @codingStandardsIgnoreEnd
-    }
-
-    /**
-     * @dataProvider invalidConfiguration
-     */
-    public function testRaisesExceptionForInvalidConfigService($config, $contains)
-    {
-        $this->container->has('config')->willReturn(true);
-        $this->container->get('config')->willReturn($config);
-
-        $factory = new TwigRendererFactory();
-        $this->setExpectedException(InvalidConfigException::class, $contains);
-        $factory($this->container->reveal());
     }
 }
