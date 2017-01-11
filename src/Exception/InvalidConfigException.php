3c69ef6d144ac1105d5a1dd7ffdca6cb717b4b81
Matthew Weier O'Phinney <matthew@zend.com>
2017-01-11 14:19:31 -0600

    Added license check to QA tools
    - Updated all license docblocks

--- src/Exception/InvalidConfigException.php
+++ src/Exception/InvalidConfigException.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @see       https://github.com/zendframework/zend-expressive for the canonical source repository
- * @copyright Copyright (c) 2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/zendframework/zend-expressive-twigrenderer for the canonical source repository
+ * @copyright Copyright (c) 2015-2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   https://github.com/zendframework/zend-expressive-twigrenderer/blob/master/LICENSE.md New BSD License
  */
 
 namespace Zend\Expressive\Twig\Exception;
